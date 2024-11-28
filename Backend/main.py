93620c3b32f1990a8c605417527ac88a3f0851f8
orevaubrufih <orevaubrufih@gmail.com>
2024-11-28 20:52:36 +0100

    added text splitter, settinngs for vectore store

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from llama_index import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 import os
+# global
+from llama_index.core.node_parser import SentenceSplitter
 
 app = FastAPI()
 
@@ -14,7 +16,15 @@ app.add_middleware(
 
 # Load your documents
 documents = SimpleDirectoryReader("data").load_data()
-index = VectorStoreIndex.from_documents(documents)
+
+
+text_splitter = SentenceSplitter(chunk_size=512, chunk_overlap=10)
+
+Settings.text_splitter = text_splitter
+
+# per-index
+index = VectorStoreIndex.from_documents(documents, transformations=[text_splitter])
+
 query_engine = index.as_query_engine()
 
 
